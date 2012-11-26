80c7f88a69c6c32d5c26833566a31d2d8f46e2b5
Roland Tanglao <roland@rolandtanglao.com>
2012-11-25 22:25:24 -0800

    oops forgot to initialize twitter oauth stuff

--- getLast3200.rb
+++ getLast3200.rb
@@ -26,6 +26,13 @@ consumer_secret = ARGV[1]
 access_token = ARGV[2]
 access_token_secret = ARGV[3]
 
+Twitter.configure do |config|
+  config.consumer_key = consumer_key
+  config.consumer_secret = consumer_secret
+  config.oauth_token = access_token
+  config.oauth_token_secret = access_token_secret
+end
+
 db = get_connection
 tweetsColl = db.collection("tweets")
 
