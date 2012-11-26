4d4a612d6252b4179807a64a57297d45540369de
Roland Tanglao <roland@rolandtanglao.com>
2012-11-25 23:39:45 -0800

    add a script to get tweets for all 14 accounts

---
+++ getTweetsForAllAccounts.sh
@@ -0,0 +1,16 @@
+./getTweets.sh translink
+./getTweets.sh travelsmart
+./getTweets.sh thecanadaline
+./getTweets.sh skytrainlady
+./getTweets.sh KenHardie
+./getTweets.sh TheBuzzer
+./getTweets.sh BePartOfThePlan
+./getTweets.sh TransLinkedMedia
+./getTweets.sh TransitPolice
+./getTweets.sh GreenestCity
+./getTweets.sh VanTransp2040
+./getTweets.sh VanGreenBldgs
+./getTweets.sh ParkBoard
+./getTweets.sh CityOfVancouver
+
+
