00ed37ad7e6eab29745d414b47f092e00af8d794
Roland Tanglao <roland@rolandtanglao.com>
2012-11-25 22:40:44 -0800

    id is no longer returned by the Twitter Gem, so get it from id_str.to_i

--- getLast3200.rb
+++ getLast3200.rb
@@ -59,7 +59,7 @@ loop do
     Twitter.user_timeline(TWITTER_SCREEN_NAME, param_hash).each do |tweet|
       t = tweet.attrs
       pp t
-      id = t["id"]
+      id = t["id_str"].to_i
       if lowest_tweet_id == 0
         lowest_tweet_id = id
       elsif id < lowest_tweet_id
