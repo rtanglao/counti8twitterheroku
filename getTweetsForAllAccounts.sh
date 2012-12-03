c17c5dc193ab31c46ecd2676eb23f05be3798e52
Roland Tanglao <roland@rolandtanglao.com>
2012-12-02 23:34:42 -0800

    do it properly

--- getTweetsForAllAccounts.sh
+++ getTweetsForAllAccounts.sh
@@ -1,16 +1,17 @@
-./getTweets.sh translink
-./getTweets.sh travelsmart
-./getTweets.sh thecanadaline
-./getTweets.sh skytrainlady
-./getTweets.sh KenHardie
-./getTweets.sh TheBuzzer
-./getTweets.sh BePartOfThePlan
-./getTweets.sh TransLinkMedia
-./getTweets.sh TransitPolice
-./getTweets.sh GreenestCity
-./getTweets.sh VanTransp2040
-./getTweets.sh VanGreenBldgs
-./getTweets.sh ParkBoard
-./getTweets.sh CityOfVancouver
+./addUser.sh translink ; ./getFollowers.sh translink ; ./add100AtATime.sh translink; ./getTweets.sh translink
+./addUser.sh travelsmart ; ./getFollowers.sh travelsmart ; ./add100AtATime.sh travelsmart; ./getTweets.sh travelsmart
+./addUser.sh thecanadaline ; ./getFollowers.sh travelsmart ; ./add100AtATime.sh travelsmart; ./getTweets.sh travelsmart
+./addUser.sh skytrainlady ; ./getFollowers.sh skytrainlady ; ./add100AtATime.sh skytrainlady; ./getTweets.sh skytrainlady
+./addUser.sh KenHardie ; ./getFollowers.sh KenHardie ; ./add100AtATime.sh KenHardie; ./getTweets.sh KenHardie
+./addUser.sh TheBuzzer ; ./getFollowers.sh TheBuzzer ; ./add100AtATime.sh TheBuzzer; ./getTweets.sh TheBuzzer
+./addUser.sh BePartOfThePlan ; ./getFollowers.sh BePartOfThePlan ; ./add100AtATime.sh BePartOfThePlan; ./getTweets.sh BePartOfThePlan
+./addUser.sh TransLinkMedia ; ./getFollowers.sh TransLinkMedia ; ./add100AtATime.sh TransLinkMedia; ./getTweets.sh TransLinkMedia
+./addUser.sh TransitPolice ; ./getFollowers.sh TransitPolice ; ./add100AtATime.sh TransitPolice; ./getTweets.sh TransitPolice
+./addUser.sh GreenestCity ; ./getFollowers.sh GreenestCity ; ./add100AtATime.sh GreenestCity; ./getTweets.sh GreenestCity
+./addUser.sh VanTransp2040 ; ./getFollowers.sh VanTransp2040 ; ./add100AtATime.sh VanTransp2040; ./getTweets.sh VanTransp2040
+./addUser.sh VanGreenBldgs ; ./getFollowers.sh VanGreenBldgs ; ./add100AtATime.sh VanGreenBldgs; ./getTweets.sh VanGreenBldgs
+./addUser.sh ParkBoard ; ./getFollowers.sh ParkBoard ; ./add100AtATime.sh ParkBoard; ./getTweets.sh ParkBoard
+./addUser.sh CityOfVancouver ; ./getFollowers.sh CityOfVancouver ; ./add100AtATime.sh CityOfVancouver; ./getTweets.sh CityOfVancouver
+
 
 
